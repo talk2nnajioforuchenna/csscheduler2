2448cd6fc5ba999b02f242dc5cd64d4b8c3a3e85
victormarius <100percentfragrance@gmail.com>
2022-03-21 00:26:22 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-app=${PWD##*/}
 
+app=csstest
 location=us-central1-docker.pkg.dev/customerservicescheduler/csstest/"$app"
 
+docker build -t ${app} .
 docker tag "$app" "$location"
-
 docker push "$location"
